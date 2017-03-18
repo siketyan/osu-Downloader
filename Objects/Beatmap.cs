d8a80b100e705a1132badaca315683a832a5300f
Siketyan <siketyan@sikeserver.com>
2017-03-18 19:55:31 +0900

    APIを追加

---
+++ Objects/Beatmap.cs
@@ -0,0 +1,156 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace osu_Downloader.Objects
+{
+    public class Beatmap
+    {
+        [JsonProperty("id")]
+        public long ID { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("artist")]
+        public string Artist { get; set; }
+
+        [JsonProperty("play_count")]
+        public long PlayCount { get; set; }
+
+        [JsonProperty("favourite_count")]
+        public long FavouriteCount { get; set; }
+
+        [JsonProperty("submitted_date")]
+        public string SubmittedDate { get; set; }
+
+        [JsonProperty("last_updated")]
+        public string LastUpdatedDate { get; set; }
+
+        [JsonProperty("ranked_date")]
+        public string RankedDate { get; set; }
+
+        [JsonProperty("creator")]
+        public string Creator { get; set; }
+
+        [JsonProperty("user_id")]
+        public long CreatorID { get; set; }
+
+        [JsonProperty("bpm")]
+        public float BPM { get; set; }
+
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("covers")]
+        public Cover Covers { get; set; }
+
+        [JsonProperty("previewUrl")]
+        public string PreviewUrl { get; set; }
+
+        [JsonProperty("tags")]
+        public string Tags { get; set; }
+
+        [JsonProperty("video")]
+        public bool HasVideo { get; set; }
+
+        [JsonProperty("has_scores")]
+        public bool HasScores { get; set; }
+
+        [JsonProperty("status"), JsonConverter(typeof(StringEnumConverter))]
+        public RankStatus Ranked { get; set; }
+
+        [JsonProperty("beatmaps")]
+        public Difficulty[] Difficulties { get; set; }
+    }
+
+    public class Cover
+    {
+        [JsonProperty("cover")]
+        public string CoverUrl { get; set; }
+
+        [JsonProperty("cover@2x")]
+        public string Cover2xUrl { get; set; }
+
+        [JsonProperty("card")]
+        public string CardUrl { get; set; }
+
+        [JsonProperty("card@2x")]
+        public string Card2xUrl { get; set; }
+
+        [JsonProperty("list")]
+        public string ListUrl { get; set; }
+
+        [JsonProperty("list@2x")]
+        public string List2xUrl { get; set; }
+    }
+
+    public class Difficulty
+    {
+        [JsonProperty("id")]
+        public long ID { get; set; }
+
+        [JsonProperty("version")]
+        public string Name { get; set; }
+
+        [JsonProperty("mode")]
+        public GameMode Mode { get; set; }
+
+        [JsonProperty("total_length")]
+        public int Length { get; set; }
+
+        [JsonProperty("difficulty_rating")]
+        public float Stars { get; set; }
+
+        [JsonProperty("cs")]
+        public float CircleSize { get; set; }
+
+        [JsonProperty("drain")]
+        public float HPDrain { get; set; }
+
+        [JsonProperty("accuracy")]
+        public float Accuracy { get; set; }
+
+        [JsonProperty("ar")]
+        public float ApproachRate { get; set; }
+
+        [JsonProperty("playcount")]
+        public long PlayCount { get; set; }
+
+        [JsonProperty("passcount")]
+        public long PassCount { get; set; }
+
+        [JsonProperty("count_circles")]
+        public long CircleCount { get; set; }
+
+        [JsonProperty("count_sliders")]
+        public long SliderCount { get; set; }
+
+        [JsonProperty("last_updated")]
+        public string LastUpdatedDate { get; set; }
+
+        [JsonProperty("status"), JsonConverter(typeof(StringEnumConverter))]
+        public RankStatus Ranked { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+    public enum RankStatus
+    {
+        Loved,
+        Qualified,
+        Approved,
+        Ranked,
+        Pending,
+        WIP,
+        Graveyard
+    }
+
+    public enum GameMode
+    {
+        Osu,
+        Taiko,
+        Fruits,
+        Mania
+    }
+}
---
+++ Utilities/API.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using osu_Downloader.Objects;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace osu_Downloader.Utilities
+{
+    public class API
+    {
+        private const string baseURL = "https://new.ppy.sh/";
+        private string sessionID;
+
+        public API(string sid)
+        {
+            sessionID = sid;
+        }
+
+        private string Get(string url, Dictionary<string, string> parameters)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.Cookie] = "osu_session=" + sessionID;
+
+                return wc.DownloadString(
+                           baseURL + url + "?"
+                               + string.Join(
+                                     "&", parameters.Select(p => p.Key + "=" + p.Value)
+                                 )
+                       );
+            }
+        }
+
+        private string Post(string url, Dictionary<string, string> parameters)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+                byte[] request = Encoding.ASCII.GetBytes(
+                                     string.Join(
+                                         "&", parameters.Select(p => p.Key + "=" + p.Value)
+                                     )
+                                 );
+                byte[] response = wc.UploadData(baseURL + url, "POST", request);
+
+                return Encoding.ASCII.GetString(response);
+            }
+        }
+
+        public Beatmap[] Search(string query,
+                                GameModeSearchOption mode = GameModeSearchOption.Osu,
+                                RankStatusSearchOption status = RankStatusSearchOption.Any,
+                                GenreSearchOption? genre = null,
+                                LanguageSearchOption? language = null,
+                                ExtraSearchOption? extra = null)
+        {
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("q", query);
+            parameters.Add("m", ((int)mode).ToString());
+            parameters.Add("s", ((int)status).ToString());
+            if (genre != null) parameters.Add("g", ((int)genre).ToString());
+            if (language != null) parameters.Add("l", ((int)language).ToString());
+            if (extra != null) parameters.Add("e", ((ExtraSearchOption)extra).GetValue());
+
+            return JsonConvert.DeserializeObject<Beatmap[]>(
+                       Get("beatmapsets/search", parameters)
+                   );
+        }
+    }
+
+    public enum GameModeSearchOption
+    {
+        Osu = 0,
+        Taiko = 1,
+        Catch = 2,
+        Mania = 3
+    }
+
+    public enum RankStatusSearchOption
+    {
+        RankedAndApproved = 0,
+        Approved = 1,
+        Favourites = 2,
+        ModRequests = 3,
+        Pending = 4,
+        Graveyard = 5,
+        MyMaps = 6,
+        Any = 7,
+        Loved = 8
+    }
+
+    public enum GenreSearchOption
+    {
+        Unspecified = 1,
+        VideoGame = 2,
+        Anime = 3,
+        Rock = 4,
+        Pop = 5,
+        Other = 6,
+        Novelty = 7,
+        HipHop = 9,
+        Electronic = 10
+    }
+
+    public enum LanguageSearchOption
+    {
+        Other = 1,
+        English = 2,
+        Japanese = 3,
+        Chinese = 4,
+        Instrumental = 5,
+        Korean = 6,
+        French = 7,
+        German = 8,
+        Swedish = 9,
+        Spanish = 10,
+        Italian = 11,
+    }
+
+    public enum ExtraSearchOption
+    {
+        Video,
+        Storyboard,
+        Both
+    }
+
+    public static class ExtraSearchOptionExt
+    {
+        private static readonly string[] values = new string[] { "0", "1", "0-1" };
+        
+        public static string GetValue(this ExtraSearchOption extra)
+        {
+            return values[(int)extra];
+        }
+    }
+}
