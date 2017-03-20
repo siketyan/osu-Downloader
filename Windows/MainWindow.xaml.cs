c926f888ca1e0dbff5e6bef9dfacc5ac0a2fb39c
Siketyan <siketyan@sikeserver.com>
2017-03-20 09:51:16 +0900

    検索結果リストのデザイン

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using osu_Downloader.Objects;
+using osu_Downloader.Utilities;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace osu_Downloader.Windows
 {
@@ -7,9 +10,24 @@ namespace osu_Downloader.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Beatmap> Result { get; set; }
+
+        private API api;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var response = API.Login("Siketyan", "**********");
+            api = new API(response.SessionID);
+            Result = new ObservableCollection<Beatmap>();
+
+            foreach (var beatmap in api.Search("ppp"))
+            {
+                Result.Add(beatmap);
+            }
+
+            DataContext = this;
         }
     }
 }
